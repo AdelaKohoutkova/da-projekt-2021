07e03329e38710354b78051c3a167d077cf533ac
Adéla Kohoutková <kohoutkova.adel@gmail.com>
2021-05-29 00:40:30 +0200

    doplněn skript u kontroly rozvazeni

--- Kontroly dat/kontrola_rozvazeni.sql
+++ Kontroly dat/kontrola_rozvazeni.sql
@@ -10,7 +10,7 @@ WHERE "ockovaci_misto_nazev" ILIKE '%národní%';
 --Národní očkovací centrum (5affc4c2-414a-4cd4-9a04-56647fa66eef) je společně s ÚVN Praha - Vakcinační centrum COVID-19 (4ea9c79c-ebac-4505-8176-7d22ca45c0d2)
 SELECT "ockovaci_misto_id"
 FROM "out_ockovaci_mista_zarizeni"
-WHERE "zarizeni_kod" = 61383082000
+WHERE "zarizeni_kod" = '61383082000'
 ;
 
 ------------------------------------------------
@@ -39,7 +39,7 @@ SELECT  "spotreba_ockovaci_mista"."datum"
 FROM "spotreba_ockovaci_mista"
 JOIN (SELECT *
       FROM "out_ockovaci_mista_zarizeni"
-      WHERE "zarizeni_kod" = 61383082000) AS "mista_vyber"
+      WHERE "zarizeni_kod" = '61383082000') AS "mista_vyber"
    ON "spotreba_ockovaci_mista"."ockovaci_misto_id" = "mista_vyber"."ockovaci_misto_id"
 JOIN "ockovaci_mista_zarizeni_rozvazeni"
     ON "ockovaci_mista_zarizeni_rozvazeni"."ockovaci_misto_id" = "spotreba_ockovaci_mista"."ockovaci_misto_id"
@@ -124,3 +124,58 @@ WHERE NOT EXISTS (select 1
                         and "cista_spotreba"."datum" =  "ockovani_zarizeni"."datum")
        AND "ockovani_zarizeni"."pocet_ockovanych" > 0
 ;
+--------------------------------------------------------------
+-- bylo potřeba ověřit rozvážení a záznamy v Břeclavy (CZ0644)
+SELECT *
+FROM "out_ockovaci_mista_zarizeni"
+WHERE "zarizeni_kod" IN (SELECT "zarizeni_kod"
+                          FROM "out_ockovaci_mista_zarizeni"
+                          WHERE "okres_nuts_kod" = 'CZ0644'
+                                  AND "ockovaci_misto_typ" <> 'ostatni')
+ORDER BY "zarizeni_kod";
+
+SELECT  "spotreba_ockovaci_mista"."datum"
+        , "ockovaci_misto_nazev"
+        , "spotreba_ockovaci_mista"."ockovaci_misto_id"
+        , "mista_vyber"."zarizeni_kod"
+        , "spotreba_ockovaci_mista"."pouzite_davky"
+        , SUM("spotreba_ockovaci_mista"."pouzite_davky") OVER (PARTITION BY "spotreba_ockovaci_mista"."ockovaci_misto_id" ORDER BY "datum") AS "pouzite_davky_misto_kumulativne"
+        , SUM("spotreba_ockovaci_mista"."pouzite_davky") OVER (PARTITION BY "mista_vyber"."zarizeni_kod", "datum") AS "pouzite_davky_zarizeni"
+        , "denni_prumer"
+        , "denni_prumer_ockujici_dny"
+        , "spotreba_ockovaci_mista"."pouzite_davky" / "pouzite_davky_zarizeni" AS "rozvazeci_pomer_datum"
+        , "rozvazeci_pomer"
+        , "rozvazeci_pomer_typ"
+        , "ockovaci_latka"
+        , "vyrobce"
+FROM "spotreba_ockovaci_mista"
+JOIN (SELECT *
+      FROM "out_ockovaci_mista_zarizeni"
+      WHERE "zarizeni_kod" = '00159816000') AS "mista_vyber"
+   ON "spotreba_ockovaci_mista"."ockovaci_misto_id" = "mista_vyber"."ockovaci_misto_id"
+JOIN "ockovaci_mista_zarizeni_rozvazeni"
+    ON "ockovaci_mista_zarizeni_rozvazeni"."ockovaci_misto_id" = "spotreba_ockovaci_mista"."ockovaci_misto_id"
+JOIN "out_ockovaci_mista_zarizeni_dalsi_info" ON "out_ockovaci_mista_zarizeni_dalsi_info"."ockovaci_misto_id" = "spotreba_ockovaci_mista"."ockovaci_misto_id"
+--WHERE "datum" BETWEEN '2021-05-17' AND '2021-05-24'
+ORDER BY  "datum", "spotreba_ockovaci_mista"."ockovaci_misto_id"
+;
+
+-- porovnání vstupní tabulky s očkováním a výstupní (po rozvážení)
+CREATE TEMPORARY TABLE "breclav_zarizeni" AS
+SELECT "zarizeni_kod"
+FROM "out_ockovaci_mista_zarizeni"
+WHERE "okres_nuts_kod" = 'CZ0644';
+
+SELECT *
+FROM (SELECT "zarizeni_kod"
+              , SUM("pocet_ockovanych_misto")
+      FROM "out_ockovani"
+      WHERE "zarizeni_kod" IN (SELECT "zarizeni_kod"
+                              FROM "breclav_zarizeni")
+      GROUP BY "zarizeni_kod") "vystup"
+LEFT JOIN (SELECT "zarizeni_kod", COUNT(1)
+            FROM "in_ockovani_ockovaci_mista"
+            WHERE "zarizeni_kod" IN (SELECT "zarizeni_kod"
+                                    FROM "breclav_zarizeni")
+            GROUP BY "zarizeni_kod") "vstup"
+ON "vystup"."zarizeni_kod" = "vstup"."zarizeni_kod";
